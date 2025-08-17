eae4424ce04ba7d37dae162e4aad9dafe5765378
Gian Peralta <gianperalta@Mac-mini-de-Gian.local>
2025-08-16 22:04:17 -0500

    Practicando python

---
+++ 01_variables.py
@@ -0,0 +1,13 @@
+
+
+my_variable_string = "Mi variable tipo cadena"
+print(my_variable_string)
+
+my_variable_int = 5
+print(my_variable_int)
+
+my_variable_bool = False
+print(my_variable_bool)
+
+#Concatenacion de variables en print
+print(my_variable_int, my_variable_string)
